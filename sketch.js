8859e61af54b53acf7ea6eb71b3bd793c6a8e16e
AnayKumar877 <signloginpeach@gmail.com>
2021-02-04 18:48:40 +0530

    created a random box

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var platform1;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -31,6 +33,7 @@ function setup(){
     log3 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
+    box6 = new Box(400,100,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
@@ -56,6 +59,7 @@ function draw(){
     log3.display();
 
     box5.display();
+    box6.display();
     log4.display();
     log5.display();
 
